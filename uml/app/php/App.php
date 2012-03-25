5a008de11d4892eb76ba66816888e213aa18d334
dpriest <wenhaoz100@gmail.com>
2012-03-25 20:03:57 +0800

    add app uml file

---
+++ uml/app/php/App.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class App extends Member {
+  private $aid;
+
+  private $name;
+
+  private $weibo;
+
+  public function __construct()
+  {
+    $this->aid = $aid;
+  }
+
+  public function set_weibo()
+  {
+  }
+
+  public function save_weibo()
+  {
+  }
+
+}
+?>
